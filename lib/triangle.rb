13c254dd4e71a32eeb349db17ba242f477a6743c
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-07 02:16:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,27 +2,16 @@ class Triangle
   attr_accessor :s1, :s2, :s3
   attr_writer :kind
   def initialize(side1, side2, side3)
-    @triangle_sides = []
-    # @triangle_sides << side1
-    # @triangle_sides << side2
-    # @triangle_sides << side3
     @s1 = side1
     @s2 = side2
     @s3 = side3
   end
-  # def valid?
-  #   sum1_2 = @triangle_sides[1] + @triangle_sides[2]
-  #   sum2_3 = @triangle_sides[2] + @triangle_sides[3]
-  #   sum3_1 = @triangle_sides[3] + @triangle_sides[1]
-  # end
-  
   def kind
     if (@s1 + @s2) <= @s3 || (@s2 + @s3) <= @s1 || (@s3 + @s1) <= @s2
       begin 
         raise TriangleError
         puts error.message
         end
-      
       elsif @s1 == @s2 && @s1 == @s3
         self.kind = :equilateral
       elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1 
@@ -31,8 +20,6 @@ class Triangle
         self.kind = :scalene
       end
   end
-  
-  
   class TriangleError < StandardError
     def message
       puts "not a triangle"
